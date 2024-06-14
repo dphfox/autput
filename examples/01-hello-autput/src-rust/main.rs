661ef32e122a9c9f4051c0d48a6b1945da99a4f2
Daniel P H Fox <danielphfox@gmail.com>
2024-06-14 11:05:19 +0100

    Hello Autput example

--- examples/01-hello-autput/src-rust/main.rs
+++ examples/01-hello-autput/src-rust/main.rs
@@ -1,6 +1,14 @@
 #[cfg(not(target_arch = "wasm32"))]
 compile_error!("This project must target WebAssembly to compile correctly.");
 
+use log::{debug, error, info, trace, warn};
+
 fn main() {
-	
+	autput::connect();
+	trace!("This is a trace message.");
+	debug!("This is a debug message.");
+	info!("This is an info message.");
+	warn!("This is a warning message.");
+	error!("This is an error message.");
+	panic!("This is a panic.");
 }
